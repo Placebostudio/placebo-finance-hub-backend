6276dc258c5e209cdcc0e15afca70e898890e3e0
erel <erelxsaar@gmail.com>
2026-08-24 16:40:23 +0300

    report update feat

--- data management/router/report-router.js
+++ data management/router/report-router.js
@@ -4,6 +4,7 @@ const { reportController } = require("../controller/report-controller.js")
 const reportRouter = new Router()
 
 reportRouter.get("/", reportController.getReports)
+reportRouter.get("/expense-ledger", reportController.getExpenseLedger)
 reportRouter.get("/:reportid", reportController.getReport)
 reportRouter.post("/", reportController.addReport)
 reportRouter.put("/:reportid", reportController.updateReport)
